194bc2843a6065d3802a55553566199cd3aa50f1
Julio Max Calderone Da Silva <julcalde@2-G-9.42heilbronn.de>
2024-12-15 21:02:17 +0100

    added header file and two .c files to start coding the game. End of the working session

---
+++ so_long.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 20:50:53 by julcalde          #+#    #+#             */
+/*   Updated: 2024/12/15 20:56:45 by julcalde         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <fcntl.h>
+# include <math.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include "MLX42/include/MLX42/MLX42.h"
+# include "libft/libft.h"
+
+#endif
